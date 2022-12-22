bc51b4193d9535b6350bf87d853d0d20c303b3ef
Abdelellah <abdelellah.dandashi@gmail.com>
2022-12-22 14:26:39 +0400

    feat: bootstrapping routes with controllers

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { TodoController } from "../controllers/todo/todo-controller";
+
+
 const router = Router();
+const todoController = new TodoController();
 
 
 router.get("/hc", (req, res) => {
@@ -9,39 +13,19 @@ router.get("/hc", (req, res) => {
 });
 
 // Display a list of TODO items
-router.get("/todo/:id", (req, res) => {
-    return res.status(200).json({
-        message: "list of items in a list"
-    });
-});
+router.get("/todo/:id", todoController.getTodoList);
 
 // Add todo list
-router.post("/todo", (req, res) => {
-    return res.status(200).json({
-        message: "Todo List Added"
-    });
-});
+router.post("/todo", todoController.addTodoList);
 
 // Add items to list
-router.post("/todo/id", (req, res) => {
-    return res.status(200).json({
-        message: "Items  added to List"
-    });
-});
+router.post("/todo/:id", todoController.AddItemsToList);
 
-//Delete items in a list 
-router.delete("/todo/:id/item/:id", (req, res) => {
-    return res.status(200).json({
-        message: "Item delete"
-    });
-});
+//Delete items from a list 
+router.delete("/todo/:id/item/:id", todoController.deleteItems);
 
 //Edit items in list 
-router.patch("/todo/:id/item/:id", (req, res) => {
-    return res.status(200).json({
-        message: "Item updated"
-    });
-});
+router.patch("/todo/:id/item/:id", todoController.editItems);
 
 
 
