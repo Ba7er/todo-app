494a755a999e09d193cf2a9e60b4143880b4fa77
Abdelellah <abdelellah.dandashi@gmail.com>
2022-12-22 13:39:00 +0400

    feat: Added todo-list  initial routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,10 +3,33 @@ const router = Router();
 
 
 router.get("/hc", (req, res) => {
-
     return res.status(200).json({
         message: "Todo App is Running!"
     });
 });
 
+
+router.post("/todo", (req, res) => {
+    return res.status(200).json({
+        message: "Todo List Added"
+    });
+});
+
+router.get("/todo/:id", (req, res) => {
+    return res.status(200).json({
+        message: "Todo List Added"
+    });
+});
+
+router.patch("/todo/:id", (req, res) => {
+    return res.status(200).json({
+        message: "Todo List Added"
+    });
+});
+
+router.delete("/todo/:id", (req, res) => {
+    return res.status(200).json({
+        message: "Todo List Added"
+    });
+});
 export default router;
