476390d422c3880e7f3f084eb38bb2ba0ae4ac6e
Abdelellah <abdelellah.dandashi@gmail.com>
2022-12-22 15:46:55 +0400

    setup: bootstrapping db to app

--- src/providers/app.ts
+++ src/providers/app.ts
@@ -1,11 +1,17 @@
-
-
 import Express from "./express";
+import { db } from "../models/index";
 
 class App {
     // Loads your Server
     public loadServer(): void {
         Express.init();
+    }
+
+    //Loads the Database Pool
+    public async loadDatabase(): Promise<void> {
+        await db.sequelize.authenticate();
+        await db.sequelize.sync({ force: false }); // @TODO: change for prod 
+        console.log('Connection has been established successfully.');
     }
 }
 
